4fc72587ae1f92f30461671cd3e0f42c1cad7cd4
ashishPurty7800 <ashishpurty7800@gmail.com>
2024-08-28 21:36:58 +0530

    Update database.js

--- server-7/config/database.js
+++ server-7/config/database.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const connect = () => {
-    mongoose.connect(process.env.MONGODB_URL, {
+const connect = async () => {
+    await mongoose.connect(process.env.MONGODB_URL, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     })
